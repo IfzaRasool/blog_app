843665ed9b3b7c06895e1931d77bf09385702913
IfzaRasool <ifzarasoolarain@gmail.com>
2022-07-07 14:20:58 +0300

    Add columns and foreign key

--- db/migrate/20220707110400_create_posts.rb
+++ db/migrate/20220707110400_create_posts.rb
@@ -1,8 +1,15 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
+      t.bigint :author_id
+      t.string :title
+      t.text :text
+      t.integer :comments_counter
+      t.integer :likes_counter
+      t.index :author_id
 
       t.timestamps
     end
+    add_foreign_key :posts, :users, column: :author_id, on_delete: :cascade
   end
 end
