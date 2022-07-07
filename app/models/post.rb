23d26f08b19f7c61079602f41cfc02fcac0fa1c1
IfzaRasool <ifzarasoolarain@gmail.com>
2022-07-07 17:38:24 +0300

    create models create method perform curd operation

---
+++ app/models/post.rb
@@ -0,0 +1,14 @@
+class Post < ApplicationRecord
+    has_many :likes
+    has_many :comments
+    belongs_to :author, class_name: 'User'
+
+    def update_counter
+      author.increment!(:posts_counter)
+    end
+  
+    def recent_comments
+      comments.order(created_at: :desc).limit(5)
+    end
+
+  end
