167c724ec9a444391c6e4d2224a1ac0e53365457
IfzaRasool <ifzarasoolarain@gmail.com>
2022-07-16 10:50:36 +0300

    fix linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
-    def current_user
-      User.find(params[:id] || params[:user_id])
-    end
-  
-    def all_users
-      User.all
-    end
+  def current_user
+    User.find(params[:id] || params[:user_id])
   end
+
+  def all_users
+    User.all
+  end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,6 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   after_save :update_counter
 
-
   def update_counter
     author.increment!(:posts_counter)
   end
